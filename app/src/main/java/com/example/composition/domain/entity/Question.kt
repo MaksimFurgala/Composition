688f1f7564b95933944a91f0599a8864d11fb7d0
FurgalaMaxim <furgala.maxim@yandex.ru>
2024-05-05 14:34:56 +0300

    added GameViewModel.kt and update resources

--- app/src/main/java/com/example/composition/domain/entity/Question.kt
+++ app/src/main/java/com/example/composition/domain/entity/Question.kt
@@ -12,4 +12,10 @@ data class Question (
     val sum: Int,
     val visibleNumber: Int,
     val options: List<Int>
-)
+) {
+    /**
+     * Правильный ответ, в зависимости
+     */
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
